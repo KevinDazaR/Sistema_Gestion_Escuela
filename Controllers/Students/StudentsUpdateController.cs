c8614744014398faf48be1264194cbcfc6ec05fe
KevinDazaR <kevindaza13@gmail.com>
2024-06-05 15:49:49 -0500

    StudentsUpdate done

---
+++ Controllers/Students/StudentsUpdateController.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PruebaLinus.Services.Students;
+using PruebaLinus.Models;
+using PruebaLinus.DTOs;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace PruebaLinus.Controllers.Students
+{
+    public class StudentsUpdateController : ControllerBase
+    {
+        private readonly IStudentsRepository _studentsRepository;
+
+        public StudentsUpdateController(IStudentsRepository studentsRepository)
+        {
+            _studentsRepository = studentsRepository;
+        }
+
+        [HttpPut]
+        [Route("students/{id}")]
+        public IActionResult Update(int id, StudentCreateDTO studentDTO)
+        {
+            // if (id != pet.Id)
+            // {
+            //     return BadRequest(new { message = "el Id de la Owner no coincide " });
+            // }
+
+            try
+            {
+                _studentsRepository.Update(id,studentDTO);
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (_studentsRepository.GetById(id) == null)
+                {
+                    return NotFound(new { message = "Owner no encontrado" });
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction(nameof(Update), new {studentDTO}, "Student updated with success.");
+        }
+    }
+}
